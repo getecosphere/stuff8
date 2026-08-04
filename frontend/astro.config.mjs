967f76c8663088c00dd5543827a8f741030d7289
Eko S. Wibowo <swdev.bali@gmail.com>
2026-08-05 05:51:52 +0700

    fix: add trailingSlash always and update detail links to use trailing slash

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -4,6 +4,7 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
+  trailingSlash: 'always',
   server: {
     port: Number(process.env.PORT) || 3000,
     host: true
