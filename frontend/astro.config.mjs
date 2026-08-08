d3e2854954912f23f7389a17e65dd5055d5b0a90
Eko S. Wibowo <swdev.bali@gmail.com>
2026-08-08 11:49:13 +0700

    switch to hybrid SSR for marketplace detail OG tags

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,10 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   trailingSlash: 'always',
+  output: 'hybrid',
+  adapter: cloudflare(),
   redirects: {
     '/signin': '/auth/signin/'
   },
